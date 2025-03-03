f47ca6867988fa077c9f9f3ac4ed15222ed6e9ea
Madalin Bogdea <madalinbogdea@icloud.com>
2025-03-03 19:30:10 +0200

    style: make footer stick to bottom

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex min-h-screen flex-col">
         <Navbar />
-        {children}
+        <main className="flex-1"> {children}</main>
         <Footer />
       </body>
     </html>
