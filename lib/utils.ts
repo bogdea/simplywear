4067e420d9059ed599503a0462564cbdc197a319
Madalin Bogdea <madalinbogdea@icloud.com>
2025-03-11 11:36:25 +0200

    refactor: extract removeFromCart function to utils.ts for reuse

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+// remove product from cart
+export const removeFromCart = async (
+  productId: string,
+  setCart: (cart: any) => void,
+  cart: any,
+) => {
+  const res = await fetch("/api/cart", {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ productId }),
+  });
+
+  if (!res.ok) {
+    const errorData = await res.json();
+    console.error("error removing item:", errorData);
+    return;
+  }
+
+  setCart(
+    Array.isArray(cart)
+      ? cart.filter((item) => item.productId !== productId)
+      : [],
+  );
+};
